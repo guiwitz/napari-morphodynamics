3c4131582cc8f9a2dd0ef1713303d11a27653568
guiwitz <guiwitz@users.noreply.github.com>
2022-01-28 18:53:13 +0100

    add first widget tests

--- src/napari_morphodynamics/_tests/test_widget.py
+++ src/napari_morphodynamics/_tests/test_widget.py
@@ -1,33 +1,74 @@
-from napari_morphodynamics import ExampleQWidget, example_magic_widget
+import pytest
+import napari_morphodynamics
+import napari_morphodynamics.napari_gui
+from morphodynamics.data import synth
 import numpy as np
+import h5py
 
-# make_napari_viewer is a pytest fixture that returns a napari viewer object
-# capsys is a pytest fixture that captures stdout and stderr output streams
-def test_example_q_widget(make_napari_viewer, capsys):
-    # make viewer and add an image layer using our fixture
+import napari_morphodynamics
+
+@pytest.fixture(scope="session")
+def dataset(tmp_path_factory):
+    """Create a dataset for testing."""
+
+    new_path = tmp_path_factory.mktemp("dataset")
+    im, sigs = synth.generate_dataset(height=100, width=100, steps=40, step_reverse=20,
+                                  displacement=0.5, radius=10, shifts=[3,7])
+
+    for ind, s in enumerate([im]+sigs):
+        h5_name = new_path.joinpath(f'synth_ch{ind+1}.h5')
+        with h5py.File(h5_name, "w") as f_out:
+            dset = f_out.create_dataset("volume", data=s, chunks=True, compression="gzip", compression_opts=1)
+
+    return new_path
+
+@pytest.fixture
+def mywidget(make_napari_viewer, napari_plugin_manager):
+    napari_plugin_manager.register(napari_morphodynamics, name='napari-morphodynamics')
     viewer = make_napari_viewer()
-    viewer.add_image(np.random.random((100, 100)))
+    _, widget = viewer.window.add_plugin_dock_widget(
+        plugin_name='napari-morphodynamics', widget_name='MorphoWidget',
+    )
+    return widget
 
-    # create our widget, passing in the viewer
-    my_widget = ExampleQWidget(viewer)
+def test_data_exist(dataset):
+    """Test that the data exists."""
+    assert dataset.joinpath("synth_ch1.h5").is_file()
+    assert dataset.joinpath("synth_ch2.h5").is_file()
+    assert dataset.joinpath("synth_ch3.h5").is_file()
 
-    # call our widget method
-    my_widget._on_click()
-
-    # read captured output and check that it's as we expected
-    captured = capsys.readouterr()
-    assert captured.out == "napari has 1 layers\n"
+def test_project_widget(mywidget):
     
-def test_example_magic_widget(make_napari_viewer, capsys):
-    viewer = make_napari_viewer()
-    layer = viewer.add_image(np.random.random((100, 100)))
+    assert isinstance(mywidget, napari_morphodynamics.napari_gui.MorphoWidget)
 
-    # this time, our widget will be a MagicFactory or FunctionGui instance
-    my_widget = example_magic_widget()
+def test_set_dataset(mywidget, dataset):
+    """Test that the dataset is updated."""
 
-    # if we "call" this object, it'll execute our function
-    my_widget(viewer.layers[0])
+    mywidget.file_list.update_from_path(dataset)
+    channels = [mywidget.segm_channel.item(i).text() for i in range(mywidget.segm_channel.count())]
+    for i in range(3):
+        assert f'synth_ch{i+1}.h5' in channels
 
-    # read captured output and check that it's as we expected
-    captured = capsys.readouterr()
-    assert captured.out == f"you have selected {layer}\n"
+    assert mywidget.param.data_folder == dataset
+
+def test_load(mywidget, dataset):
+    # set path, set channels to use
+    mywidget.file_list.update_from_path(dataset)
+    channels = [mywidget.segm_channel.item(i).text() for i in range(mywidget.segm_channel.count())]
+    mywidget.segm_channel.setCurrentRow(channels.index('synth_ch1.h5'))
+    for i in range(mywidget.signal_channel.count()):
+        if mywidget.signal_channel.item(i).text() in ['synth_ch2.h5', 'synth_ch3.h5']:
+            mywidget.signal_channel.item(i).setSelected(True)
+
+    # load the data
+    mywidget._on_load_dataset()
+
+    # check that the data is loaded
+    assert mywidget.data.dims == (100,100)
+    assert mywidget.param.morpho_name == 'synth_ch1.h5'
+
+    # check that the data is added to viewer
+    assert len(mywidget.viewer.layers) == 3
+    assert mywidget.viewer.layers[0].name == 'synth_ch1.h5'
+    assert mywidget.viewer.layers[1].name == 'signal synth_ch3.h5'
+    assert mywidget.viewer.layers[2].name == 'signal synth_ch2.h5'
